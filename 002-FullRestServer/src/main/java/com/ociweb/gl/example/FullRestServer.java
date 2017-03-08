5a570636abbdb2355a2a3f8343f6f7e5e1955705
Nathan Tippy <nathan@tippy.name>
2017-03-08 16:17:12 -0600

    added static files for file server
    added new 002 full test

--- 002-FullRestServer/src/main/java/com/ociweb/gl/example/FullRestServer.java
+++ 002-FullRestServer/src/main/java/com/ociweb/gl/example/FullRestServer.java
@@ -4,13 +4,15 @@ import com.ociweb.gl.api.Builder;
 import com.ociweb.gl.api.GreenApp;
 import com.ociweb.gl.api.GreenRuntime;
 
-public class SimpleRestServer implements GreenApp {
+public class FullRestServer implements GreenApp {
 
 	int REST_ROUTE_1;
 	int REST_ROUTE_2;
+	int STATIC_FILES_ROUTE;
+	String pathToIndex="";
 	
 	public static void main(String[] args) { //All green lightning apps start with this main which passes in a GreenApp instance.
-		 GreenRuntime.run(new SimpleRestServer());
+		 GreenRuntime.run(new FullRestServer());
 	}
 	
 	@Override
@@ -22,23 +24,25 @@ public class SimpleRestServer implements GreenApp {
 		int bindPort = 8081;
 		
 		builder.enableServer(isTLS, isLarge, bindHost, bindPort);//turn on the web server
-				
+		
+		builder.parallelism(4); //multiple instances of module will be running.
+		
 		REST_ROUTE_1 = builder.registerRoute("/groovyadd/%i%./%i%.");//these routes take 2 decimal (eg floating point) numbers
 		REST_ROUTE_2 = builder.registerRoute("/add/%i%./%i%.");
 		
-		//REST_ROUTE_1 = builder.registerRoute("/groovyadd/%i/%i");//these routes only except simple integers
-		//REST_ROUTE_2 = builder.registerRoute("/add/%i/%i");
-				
+		
+		STATIC_FILES_ROUTE = builder.registerRoute("/%b"); //define the route to wich the module will respond.
+			
 	}
 
 	@Override
 	public void declareBehavior(GreenRuntime runtime) {		
-		runtime.addRestListener(new MathUnit(runtime, REST_ROUTE_1, REST_ROUTE_2)); //takes either route
+		runtime.addFileServer(pathToIndex, STATIC_FILES_ROUTE); //This is needed to capure unknonwn routes and return 404
 	}
 
 	@Override
 	public void declareParallelBehavior(GreenRuntime runtime) {	
-		//NOT NEEDED FOR THIS EXAMPLE
+		runtime.addRestListener(new MathUnit(runtime, REST_ROUTE_1, REST_ROUTE_2)); //takes either route
 	}
 	
 
---
+++ 002-FullRestServer/src/test/java/com/ociweb/gl/example/ServerTest.java
@@ -0,0 +1,62 @@
+package com.ociweb.gl.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.ociweb.gl.api.GreenRuntime;
+
+public class ServerTest {
+
+	
+	private static GreenRuntime runtime;
+
+	@BeforeClass
+	public static void start() {
+		runtime = GreenRuntime.run(new FullRestServer());//NOTE: it would be nice if this could pick its own port in case of collision while testing.
+	}
+	
+	@AfterClass
+	public static void end() {
+		runtime.shutdownRuntime();
+	}
+	
+	@Test
+	public void serverTest() {
+		
+		try {
+			URL url = new URL("http://127.0.0.1:8081/groovyadd/2.3/7.52");
+			
+			InputStream stream = url.openStream();
+			
+			Scanner scanner = new Scanner(stream);
+			
+			assertTrue(scanner.hasNextLine());
+			
+			String v = scanner.nextLine();
+			//System.out.println(v);
+
+			assertEquals("{\"x\":2.3,\"y\":7.52,\"groovySum\":9.82}", v);
+						
+			
+		} catch (MalformedURLException e) {			
+			e.printStackTrace();
+			fail();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+	}
+	
+}
